effb78fef239db7eda9ef4877817fa1f94440448
Anvesh Reddy <p.anveshreddy9@gmail.com>
2026-04-08 11:27:54 +0530

    ETL pipeline

---
+++ networksecurity/exception/exception.py
@@ -0,0 +1,16 @@
+import sys
+from networksecurity.logging import logger
+class NetworkSecurityException(Exception):
+    def __init__(self, error_message,error_details:sys):
+        self.error_message=error_message
+        _,_,exc_tb=error_details.exc_info()
+
+        self.lineno=exc_tb.tb_lineno
+        self.filename=exc_tb.tb_frame.f_code.co_filename
+    
+    def __str__(self):
+        return f"Error occurred in script: {self.filename} at line number: {self.lineno} with error message: {self.error_message}"
+    
+
+
+    
