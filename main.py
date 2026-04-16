8d817a1553f5453062d5b119489cf138d4bf9609
Anvesh Reddy <p.anveshreddy9@gmail.com>
2026-04-16 11:59:57 +0530

    Final commit

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.components.data_validation import DataValidation
 from networksecurity.components.data_transformation import DataTransformation
-from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig
+from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig,DataValidationConfig,DataTransformationConfig,ModelTrainerConfig
 import sys
+from networksecurity.components.model_trainer import ModelTrainer
 
 if __name__=="__main__":
     try:
@@ -27,6 +28,16 @@ if __name__=="__main__":
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         logging.info("Data transformation completed successfully.")
         print(data_transformation_artifact)
+
+        logging.info("Initializing ModelTrainer class successfully.")
+        model_trainer_config=ModelTrainerConfig(training_pipeline_config=trainingpipleineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        logging.info("Model training completed successfully.")
+        print(model_trainer_artifact)
+
+
+        
 
 
 
